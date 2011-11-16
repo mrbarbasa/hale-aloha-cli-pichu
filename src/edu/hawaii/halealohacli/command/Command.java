1a1d98f9d2421178ddd5887dcdc155cb90e71141
rylan.kwock <rylan.kwock@gmail.com>
2011-11-16 01:02:21 +0000

    [Issue 7] Added methods for commands to implement.

--- src/edu/hawaii/halealohacli/command/Command.java
+++ src/edu/hawaii/halealohacli/command/Command.java
@@ -1,10 +1,12 @@
 package edu.hawaii.halealohacli.command;
 
 /**
- * 
- * 
+ * Interface for program commands.
  * @author Team Pichu
  */
 public interface Command {
-
+  
+  void run();
+  String getOutput();
+  String description();
 }
