19e10399729dfa0a375b7a0c8a2b2aa130ce0d4d
h.wk.cheng@gmail.com <h.wk.cheng@gmail.com@f8aeccd0-5101-023c-9e15-fd06835d1609>
2011-11-18 00:25:02 +0000

    [Issue 16]
    Modified TestDailyEnergy class to check if energy returned is greater than or less than 0.

--- trunk/src/edu/hawaii/halealohacli/test/TestDailyEnergy.java
+++ trunk/src/edu/hawaii/halealohacli/test/TestDailyEnergy.java
@@ -1,21 +1,33 @@
 package edu.hawaii.halealohacli.test;
 
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
+import edu.hawaii.halealohacli.command.DailyEnergy;
 
 /**
- * 
+ * Test the DailyEnergy class.
  * 
  * @author Team Pichu
  */
 public class TestDailyEnergy {
-
+  
+  private String testSource = "Ilima-04-lounge";
+  private String testDate = "2011-10-26";
+  
   /**
+   * Test the results returned.
    * 
+   * @throws Exception - error.
    */
   @Test
-  public void test() {
-    assertSame("Placeholder: Does 1 + 1 = 2?", 1 + 1, 2);
+  public void testGetDailyEnergy() throws Exception {
+    
+    DailyEnergy energy = new DailyEnergy(testSource, testDate);
+    
+    assertTrue("Testing daily energy", energy.getEnergy() > 0);
+    assertFalse("Test daily energy less than 0", energy.getEnergy() < 0);
   }
+  
 
 }
