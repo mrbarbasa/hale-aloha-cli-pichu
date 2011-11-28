72e41a291572490beb8bb3d8e978d2467656c16d
h.wk.cheng <h.wk.cheng@gmail.com>
2011-11-28 05:37:21 +0000

    [Issue 19]
    Implemented TestRankTowers class to test if either of the dates entered can't retrieve data since it is too early to know the data.

--- src/edu/hawaii/halealohacli/test/TestRankTowers.java
+++ src/edu/hawaii/halealohacli/test/TestRankTowers.java
@@ -1,7 +1,11 @@
 package edu.hawaii.halealohacli.test;
 
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertEquals;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 import org.junit.Test;
+import edu.hawaii.halealohacli.command.RankTowers;
 
 /**
  * 
@@ -11,11 +15,24 @@ import org.junit.Test;
 public class TestRankTowers {
 
   /**
+   * Test if the dates entered actually allows
+   * the retrieval of data from the sources.
    * 
+   * @throws Exception - error.
    */
   @Test
-  public void test() {
-    assertSame("Placeholder: Does 1 + 1 = 2?", 1 + 1, 2);
+  public void test() throws Exception {
+    
+    Calendar today = Calendar.getInstance(Locale.US);
+    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+    //Adding one day to the current day
+    today.add(Calendar.DATE, 1);
+    String dayAhead = df.format(today.getTimeInMillis());
+    RankTowers test = new RankTowers("2011-11-23", dayAhead);
+    test.run();
+    String message = "Too early to retrieve data for the dates entered";
+    
+    assertEquals("Test dates", message, test.getOutput());
   }
 
 }
