80c2e0ed8ddf612d87d6930534527193fbc55bc9
mrbarbasa <mrbarbasa@f8aeccd0-5101-023c-9e15-fd06835d1609>
2011-11-28 05:52:24 +0000

    [Issue 13] Quit class is Done, for now. [Issue 20] TestHelp class is Done, for now. [Issue 21] TestQuit class is Done, for now.

--- src/edu/hawaii/halealohacli/test/TestQuit.java
+++ src/edu/hawaii/halealohacli/test/TestQuit.java
@@ -2,20 +2,25 @@ package edu.hawaii.halealohacli.test;
 
 import static org.junit.Assert.assertSame;
 import org.junit.Test;
+import edu.hawaii.halealohacli.command.Quit;
 
 /**
- * 
+ * Tests the Quit class.
  * 
  * @author Team Pichu
  */
 public class TestQuit {
 
   /**
+   * Tests if the quit command returns "quit".
    * 
+   * @throws Exception Doesn't really throw an exception.
    */
   @Test
-  public void test() {
-    assertSame("Placeholder: Does 1 + 1 = 2?", 1 + 1, 2);
+  public void testQuit() throws Exception {
+    Quit quit = new Quit();
+    quit.run();
+    assertSame("Testing quit", quit.getOutput(), "quit");
   }
 
 }
